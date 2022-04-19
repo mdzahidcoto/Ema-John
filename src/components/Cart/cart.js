c70b36d84d4c73af659ed21349c76176b79f3b78
Md. Zahid Sarker <84657860+mdzahidcoto@users.noreply.github.com>
2022-04-19 16:57:56 +0600

    cart primary done with total and others

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -2,10 +2,38 @@ import React from 'react';
 import './cart.css';
 
 const Cart = (props) => {
+    const cart = props.cart;
+    // Total price count
+    const totalPrice = cart.reduce((total, product) => total + product.price, 0);
+    // Shipping charge of product
+    let shipping = 0;
+    if(totalPrice > 500){
+        shipping = 0;
+    }
+    else if(totalPrice > 75){
+        shipping = 4.99;
+    }
+    else if(totalPrice > 0) { 
+        shipping = 7.99;
+    }
+    // Tax on product price
+    const tax = totalPrice * 0.75;
+    // over all total
+    const grandTotal = totalPrice + shipping + tax;
+
+    // function to fixed number 2 and return number
+    const fixedNum = num => {
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+
     return (
         <div>
-            <h3>Cart containers</h3>
-            <h4>Item Ordered: {props.cart.length}</h4>
+            <h4>Item Ordered: {cart.length}</h4>
+            <p>Product Price: ${fixedNum(totalPrice)}</p>
+            <p><small>Shipping Charger: ${shipping}</small></p>
+            <p><small>Tax + VAT: ${fixedNum(tax)}</small></p>
+            <p><strong>Total Price:</strong> ${fixedNum(grandTotal)}</p>   
         </div>
     );
 };
